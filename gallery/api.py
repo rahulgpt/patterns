79663d7a9d58d33b4cbf71eb6f86eb2df13ae21f
Rahul Gupta <rahulr09gupta@gmail.com>
2022-12-22 19:30:38 +0530

    feat(backend): cache gallery viewset for 24h

--- gallery/api.py
+++ gallery/api.py
@@ -1,9 +1,10 @@
 from .models import GalleryImage
 from rest_framework import viewsets, permissions
 from .serializers import GallerySerializer
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 
 # Gallery Viewset
-
 
 class GalleryViewSet(viewsets.ModelViewSet):
     queryset = GalleryImage.objects.all().order_by('order')
@@ -11,3 +12,8 @@ class GalleryViewSet(viewsets.ModelViewSet):
         permissions.AllowAny
     ]
     serializer_class = GallerySerializer
+
+    @method_decorator(cache_page(60 * 60 * 24))
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
